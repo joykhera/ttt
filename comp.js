fa7b7fc3bbbd25a4f3b96a8df223f307984d85f8
manticore999 <73572578+manticore999@users.noreply.github.com>
2021-01-26 21:04:19 -0500

    a

--- comp.js
+++ comp.js
@@ -3,13 +3,6 @@ import { x } from "./shapes.js"
 import { player } from "./player.js"
 
 export const comp = {
-    turn: false,
-
-    // isTurn(){
-    //     if(player.turn) this.turn = false
-    //     else if(!player.turn) this.turn = true
-    // },
-
     checkPlaced(zone){
         if(!zone.placed){
             zone.draw.x = true
@@ -29,9 +22,7 @@ export const comp = {
 
     draw(ctx){
         for(const zone of zonearr){
-            // this.isTurn()
             if(!player.turn) this.checkDraw(ctx, zone)
-            
         }
     }
 }
